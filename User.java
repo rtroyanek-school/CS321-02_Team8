4d9e0dee755a3e412c706e4dfcc61fc5e5cd8489
RebeccaKigwana <bkigwana@gmail.com>
2024-11-21 14:32:58 -0600

    Update User.java

--- User.java
+++ User.java
@@ -3,59 +3,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.blackjackgame;
-
 /**
- *
- * @author rtroyanek
- * Represents the player's hand. Responsible for drawing  cards and adding
- * them to the player's hand.
+ * Represents the user (player) in a Blackjack game.
+ * Manages the player's hand and actions like drawing cards.
  */
 public class User {
-    private Hand hand;
-    private Deck deck;
-    
+    private Hand hand; // The player's hand of cards
+
     /**
-     * A list to hold the player's hand
+     * Constructs a User with an empty hand.
      */
-    public User(Deck deck) {
+    public User() {
         this.hand = new Hand();
-        this.deck = deck;
     }
-    
+
     /**
-     * Draw a card from the deck and add it to the player's hand
+     * Draws a card from the deck and adds it to the player's hand.
+     *
+     * @param deck the deck from which to draw the card
      */
     public void drawCard(Deck deck) {
-        Card drawnCard = deck.dealCard();
-        if (drawnCard != null){
-            hand.addCard(drawnCard);
-            System.out.println("You drew: " + drawnCard);
-            System.out.println("Your hand: " + hand + " (Total: " + hand.getHandValue() + ")");
-        }
+        Card card = deck.dealCard();
+        hand.addCard(card);
     }
-    
+
     /**
-     * Returns the list of cards in the player's hand 
-     * 
-     * @return list of cards in hand
+     * Returns the total value of the player's hand.
+     *
+     * @return the value of the player's hand
+     */
+    public int getHandValue() {
+        return hand.getValue();
+    }
+
+    /**
+     * Returns the player's hand.
+     *
+     * @return the player's hand
      */
     public Hand getHand() {
         return hand;
     }
-    
+
     /**
-     * Returns value of all cards in player's hand
-     * 
-     * @return value of the hand
-     */
-    public int getHandValue() {
-        return hand.getHandValue();
-    }
-    
-    /**
-     * Resets the player's hand to be prepared for the next round
+     * Resets the player's hand to an empty hand for a new round.
      */
     public void resetHand() {
-        hand.clear();
+        hand = new Hand();
     }
 }
+
