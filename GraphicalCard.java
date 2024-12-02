2655facbf2f5d3085274917cae0ad15424da1441
rtroyanek-school <rt0047@uah.edu>
2024-12-02 01:54:46 -0600

    Update GraphicalCard.java

--- GraphicalCard.java
+++ GraphicalCard.java
@@ -23,35 +23,10 @@ public class GraphicalCard {
         this.suit = suit;
     }
 
-    public void draw(Graphics g, Component component, int x, int y) {
-        if (findImage()) {
-            int cx;    // x-coord of upper left corner of the card inside cardImage
-            int cy;    // y-coord of upper left corner of the card inside cardImage
-            switch (suit) {
-                case CLUBS:    cy = 0; break;
-                case DIAMONDS: cy = 123; break;
-                case HEARTS:   cy = 2 * 123; break;
-                case SPADES:   cy = 3 * 123; break;
-                default:       cy = 4 * 123;   // Joker
-            }
-            if (suit == JOKER) {
-                if (value == 1)
-                    cx = 0;
-                else
-                    cx = 79;
-            } else {
-                cx = (value - 1) * 79;
-            }
-            g.drawImage(cardImage, x, y, x + 79, y + 123, cx, cy, cx + 79, cy + 123, component);
-        } else {
-            g.setColor(Color.WHITE);
-            g.fillRect(x, y, 79, 120);
-            g.setColor(Color.BLACK);
-            g.drawRect(x, y, 78, 119);
-            g.drawString(getValueAsString(), x + 10, y + 30);
-            g.drawString("of", x + 20, y + 60);
-            g.drawString(getSuitAsString(), x + 10, y + 90);
-        }
+    public void draw(Graphics g, JPanel panel, int x, int y) {
+        String cardPath = card.getImagePath();
+        Image cardImage = new ImageIcon().getImage();
+        g.drawImage(cardImage, x, y, 80, 120, panel); 
     }
 
     private boolean findImage() {
